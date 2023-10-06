2b9724ffc4cb079f84d89742c4eb5968ad202bdd
sweetyhank <145088436+sweetyhank@users.noreply.github.com>
2023-10-06 12:09:59 +0800

    Add files via upload

---
+++ week04-1.py
@@ -0,0 +1,2 @@
+1.for i in range(10):
+  print('Hello',i)
---
+++ week04-4.py
@@ -0,0 +1,11 @@
+class Solution:
+    def arraySign(self, nums: List[int]) -> int:
+        
+        ans = 1        #一開始的答案是正確的
+        for n in nums: #一序取n出來
+            ans *= n   #答案 乘n
+
+    
+        if ans>0: return 1
+        if ans<0: return -1
+        if ans==0: return 0
