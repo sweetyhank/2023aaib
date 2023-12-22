531348d7dfeaba01cee5f88363fd2b5af1a3863c
sweetyhank <12751090@me.mcu.edu.tw>
2023-12-22 11:49:53 +0800

    week15

---
+++ week15/week15-2-b.py
@@ -0,0 +1,17 @@
+class Solution:
+    def maxScore(self, s: str) -> int:
+        N=len(s)#長度
+        zero=0#等一下也要
+        one=0#想找出全部的1
+        for i in range(N):
+            if s[i]=='1':one+=1#如果是1，先全部加起來  
+#print('一開始的時候，都在右邊，統計結果','zero',zero,'one',one)
+        ans=0
+        for i in range(N-1):
+            if s[i]=='0':
+                zero+=1
+            if s[i]=='1':
+                one-=1
+             #print('中間過程中''zero',zero,'one',one)
+            ans = max(ans,zero+one)
+        return ans
